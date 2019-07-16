4555515a851b03086eb0a9025aadec8c9dbeb6af
Milan <milan.dhar@gmail.com>
2019-07-15 17:58:29 -0700

    rendered world map

---
+++ src/chloroplethMap.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Datamap from 'datamaps/dist/datamaps.world.min.js';
+import d3 from 'd3';
+import WorldJson from './World.topo.json';
+
+class ChloroplethMap extends Component {
+
+    componentDidMount() {
+        // Datamaps expect data in format:
+        // { "USA": { "fillColor": "#42a844", numberOfWhatever: 75},
+        //   "FRA": { "fillColor": "#8dc386", numberOfWhatever: 43 } }
+        let dataset = {};
+
+        // We need to colorize every country based on "numberOfWhatever"
+        // colors should be uniq for every value.
+        // For this purpose we create palette(using min/max this.props.data-value)
+        let onlyValues = this.props.data.map(function (obj) { return obj[1]; });
+        let minValue = Math.min.apply(null, onlyValues),
+            maxValue = Math.max.apply(null, onlyValues);
+
+        // create color palette function
+        // color can be whatever you wish
+        let paletteScale = d3.scale.linear()
+            .domain([minValue, maxValue])
+            .range(["#EFEFFF", "#02386F"]); // blue color
+
+        // fill dataset in appropriate format
+        this.props.data.forEach(function (item) { //
+            // item example value ["USA", 70]
+            let iso = item[0],
+                value = item[1];
+            dataset[iso] = { numberOfThings: value, fillColor: paletteScale(value) };
+        });
+
+        let height = 168;
+        let width = 360;
+
+        let map = new Datamap({
+            element: document.getElementById('chloropleth_map'),
+            scope: 'world',
+            geographyConfig: {
+                popupOnHover: true,
+                highlightOnHover: true,
+                borderColor: '#444',
+                highlightBorderWidth: 1,
+                borderWidth: 0.5,
+                dataJson: WorldJson,
+                popupTemplate: function (geo, data) {
+                    // don't show tooltip if country don't present in dataset
+                    if (!data) { return; }
+                    // tooltip content
+                    return ['<div class="hoverinfo">',
+                            '<strong>', geo.properties.name, '</strong>',
+                            '<br>Count: <strong>', data.numberOfThings, '</strong>',
+                            '</div>'].join('');
+                    }
+            },
+            fills: {
+                HIGH: '#afafaf',
+                LOW: '#123456',
+                MEDIUM: 'blue',
+                UNKNOWN: 'rgb(0,0,0)',
+                defaultFill: '#eee'
+            },
+            data: dataset,
+            // projection: 'mercator'
+            setProjection: function (element) {
+                var projection = d3.geo.mercator()
+                    .center([0, 0]) // always in [East Latitude, North Longitude]
+                    .scale(250)
+                    .translate([element.offsetWidth / 3, element.offsetHeight / 3]);
+
+                var path = d3.geo.path().projection(projection);
+                return { path: path, projection: projection };
+            }
+        });
+    }
+    render() {
+        return (
+            <div id="chloropleth_map" style={{
+                height: "180vh",
+                width: "180vw",
+            }}></div>
+        );
+    }
+}
+
+export default ChloroplethMap;
