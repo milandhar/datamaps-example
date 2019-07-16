64b096c51ad7ce3680d38ba1cf018eb7a3fca289
Milan <milan.dhar@gmail.com>
2019-07-16 11:09:30 -0700

    starting zoome

--- src/chloroplethMap.js
+++ src/chloroplethMap.js
@@ -1,15 +1,93 @@
 import React, { Component } from 'react';
 import Datamap from 'datamaps/dist/datamaps.world.min.js';
-import d3 from 'd3';
+import d3, { selection } from 'd3';
 import WorldJson from './World.topo.json';
 
 class ChloroplethMap extends Component {
+
+    constructor(){
+      super()
+      let height = "180vh"
+      let width = "180vw"
+      let active = d3.select(null)
+    }
+
+
 
     componentDidMount() {
         // Datamaps expect data in format:
         // { "USA": { "fillColor": "#42a844", numberOfWhatever: 75},
         //   "FRA": { "fillColor": "#8dc386", numberOfWhatever: 43 } }
+        let element = document.getElementById('chloropleth_map');
+        let projection = d3.geo.mercator()
+            .center([0, 0]) // always in [East Latitude, North Longitude]
+            .scale(250)
+            .translate([element.offsetWidth / 3, element.offsetHeight / 3]);
+        let path = d3.geo.path().projection(projection);
+        let svg = d3.select("body").append("svg")
+          .attr("width", this.width)
+          .attr("height", this.height)
+          .on("click", this.stopped(), true);
+
+        svg.append("rect")
+          .attr("class", "background")
+          .attr("width", this.width)
+          .attr("height", this.height)
+          .on("click", reset);
+
+          var g = svg.append("g");
+
+          svg
+            .call(zoom)
+            d3.json("https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-50m.json", function(error, world) {
+          if (error) throw error;
+
+          g.selectAll("path")
+              .data(WorldJson.feature(world, world.objects.countries).features)
+            .enter().append("path")
+              .attr("d", path)
+              .attr("class", "feature")
+              .on("click", clicked);
+
+          g.append("path")
+              .datum(WorldJson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+              .attr("class", "mesh")
+              .attr("d", path);
+        });
+
+        function clicked(d) {
+          if (this.active.node() === this) return reset();
+          this.active.classed("active", false);
+          this.active = d3.select(this).classed("active", true);
+
+          var bounds = path.bounds(d),
+              dx = bounds[1][0] - bounds[0][0],
+              dy = bounds[1][1] - bounds[0][1],
+              x = (bounds[0][0] + bounds[1][0]) / 2,
+              y = (bounds[0][1] + bounds[1][1]) / 2,
+              scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / this.width, dy / this.height))),
+              translate = [this.width / 2 - scale * x, this.height / 2 - scale * y];
+
+          svg.transition()
+              .duration(750)
+              // .call(zoom.translate(translate).scale(scale).event); // not in d3 v4
+              .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) ); // updated for d3 v4
+        }
+
+        function reset() {
+            this.active.classed("active", false);
+            this.active = d3.select(null);
+
+            svg.transition()
+                .duration(750)
+                // .call( zoom.transform, d3.zoomIdentity.translate(0, 0).scale(1) ); // not in d3 v4
+                .call( zoom.transform, d3.zoomIdentity ); // updated for d3 v4
+          }
+
+
         let dataset = {};
+        // zoom code from: https://bl.ocks.org/piwodlaiwo/90777c94b0cd9b6543d9dfb8b5aefeef
+        let zoom = selection.call(d3.zoom().on("zoom", this.zoomed()));
 
         // We need to colorize every country based on "numberOfWhatever"
         // colors should be uniq for every value.
@@ -32,11 +110,12 @@ class ChloroplethMap extends Component {
             dataset[iso] = { numberOfThings: value, fillColor: paletteScale(value) };
         });
 
-        let height = 168;
-        let width = 360;
+        // let height = 168;
+        // let width = 360;
 
         let map = new Datamap({
-            element: document.getElementById('chloropleth_map'),
+            // element: document.getElementById('chloropleth_map'),
+            element: element,
             scope: 'world',
             geographyConfig: {
                 popupOnHover: true,
@@ -65,21 +144,35 @@ class ChloroplethMap extends Component {
             data: dataset,
             // projection: 'mercator'
             setProjection: function (element) {
-                var projection = d3.geo.mercator()
-                    .center([0, 0]) // always in [East Latitude, North Longitude]
-                    .scale(250)
-                    .translate([element.offsetWidth / 3, element.offsetHeight / 3]);
-
-                var path = d3.geo.path().projection(projection);
+                // var projection = d3.geo.mercator()
+                //     .center([0, 0]) // always in [East Latitude, North Longitude]
+                //     .scale(250)
+                //     .translate([element.offsetWidth / 3, element.offsetHeight / 3]);
+                //
+                // var path = d3.geo.path().projection(projection);
                 return { path: path, projection: projection };
             }
         });
     }
+
+    zoomed(){
+      let g = document.getElementsByTagName("g")[0]
+      console.log(g)
+      g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+      // g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"); // not in d3 v4
+      g.attr("transform", d3.event.transform); // updated for d3 v4
+    }
+
+    // If the drag behavior prevents the default click,
+    // also stop propagation so we don’t click-to-zoom.
+    stopped() {
+      if (d3.event.defaultPrevented) d3.event.stopPropagation();
+    }
     render() {
         return (
             <div id="chloropleth_map" style={{
-                height: "180vh",
-                width: "180vw",
+                height: this.height,
+                width: this.width,
             }}></div>
         );
     }
